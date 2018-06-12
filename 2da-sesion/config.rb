a3fb3be2a397c00966f83a9aca181d5a404a7532
acastemoreno <acastemoreno@gmail.com>
2018-06-12 12:32:50 -0500

    2da sesion y parte de la 3era

--- 2da-sesion/config.rb
+++ 2da-sesion/config.rb
@@ -30,6 +30,6 @@ output_style = :compressed #:expanded or :nested or :compact or :compressed
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+#sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
